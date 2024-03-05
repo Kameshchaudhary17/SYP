33849082cc5198663eae9f98916ef9801eb294cf
Anir Jung Thapa <anir234thapa@gmail.com>
2024-03-05 18:42:32 +0545

    ..

--- Server/index.js
+++ Server/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors')
 
 const signupRoute = require('./src/controllers/signup')
 const loginRoute = require('./src/controllers/login')
@@ -6,6 +7,7 @@ const loginRoute = require('./src/controllers/login')
 const app = express()
 
 app.use(express.json());
+app.use(cors())
 
 app.post("/signup", signupRoute)
 app.post("/signup", loginRoute)
